7473806a1e876949279b58ee987c193925180c34
Luís Felipe Viana <lfvianadf@hotmail.com>
2026-03-01 11:33:03 -0300

    Alterações no CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import 'dotenv/config'; // <-- A mágica acontece aqui
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: 'https://admin.blogdosantana.com.br', // Use o domínio exato que está no seu header de Origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  allowedHeaders: 'Content-Type, Accept, Authorization',
+})
   await app.listen(process.env.PORT ?? 3000);
-  app.enableCors();
 }
 bootstrap();
