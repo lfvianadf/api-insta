a779fee08bfaa96c0b4d959cfce510b764d5691e
Luís Felipe Viana <lfvianadf@hotmail.com>
2026-03-02 10:18:33 -0300

    Alterações no Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config'; // <-- A mágica acontece aqui
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: 'https://admin.blogdosantana.com.br', // Use o domínio exato que está no seu header de Origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  allowedHeaders: 'Content-Type, Accept, Authorization',
-})
+    origin: 'https://admin.blogdosantana.com.br', // Use o domínio exato que está no seu header de Origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  })
+  app.use(json({ limit: '150mb' }));
+  app.use(urlencoded({ limit: '150mb', extended: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
